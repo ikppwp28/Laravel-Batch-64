34b4badea108501af248a980cc514d11ce2f3db6
username anda <email anda>
2025-02-28 07:50:21 +0700

    Menambahkan Tugas 14 terkait migration data

---
+++ Tugas 12/task-app/database/migrations/2025_02_27_093436_create_books_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('summary');
+            $table->string('image', 255);
+            $table->integer('stok');
+            $table->foreignId('genre_id')->constrained('genres')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
